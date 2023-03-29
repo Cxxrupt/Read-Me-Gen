54c79d70ea3dcd9763bb2c64ea8bc69691aa0fd4
Cxxrupt <122586350+Cxxrupt@users.noreply.github.com>
2023-03-29 12:03:07 -0700

    Index.js prompt and init

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,61 @@
-// TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const { writeFile } = require('fs');
+const inquirer = require('inquirer');
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Create an array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Please provide a description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Please provide installation instructions for your project:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Please provide usage information for your project:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Please choose a license for your project:',
+    choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Please provide contribution guidelines for your project:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Please provide test instructions for your project:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+  },
+  {
+    type: 'input',
+    name: 'username',
+    message: 'What is your GitHub username?',
+  },
+]
 
-// TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
+async function init() {
+  const answers = await inquirer.prompt(questions);
+  console.log(answers);
+}
+
 init();
